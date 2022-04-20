cc06478bce6e707e3fe50cedf1d1c8095be100c2
benha <benharris4848@gmail.com>
2022-04-20 13:50:11 -0400

    Continue implementing PublicationsScreen.kt

--- app/src/main/java/com/example/volume_android_revamp/ui/PublicationsScreen.kt
+++ app/src/main/java/com/example/volume_android_revamp/ui/PublicationsScreen.kt
@@ -1,20 +1,21 @@
 package com.example.volume_android_revamp.ui
 
-import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import com.example.volume_android_revamp.R
 import com.example.volume_android_revamp.viewmodels.PublicationTabViewModel
 
@@ -22,24 +23,27 @@ import com.example.volume_android_revamp.viewmodels.PublicationTabViewModel
 fun PublicationScreen(publicationTabViewModel: PublicationTabViewModel){
     val context = LocalContext.current
     Column{
-        PublicationsTitle()
-        FollowingPubs()
+        publicationsTitle()
+        LazyColumn{
+            item{ followingPubs() }
+            item{ morePublications() }
+        }
     }
 
 }
 
 @Composable
-fun PublicationsTitle(){
+fun publicationsTitle(){
     Row {
         Image(painter = painterResource(R.drawable.publications_title), contentDescription = stringResource(R.string.publications_title))
     }
 }
 
 @Composable
-fun FollowingPubs(){
+fun followingPubs(){
     Column {
         Text(stringResource(id = R.string.following), color = Color.Black, style = MaterialTheme.typography.subtitle1)
-        Image(painter = painterResource(id = R.drawable.ic_following_line), contentDescription = stringResource(
+        Image(painter = painterResource(id = R.drawable.ic_following_pub_line), contentDescription = stringResource(
             id = R.string.underline
         ))
         LazyRow{
@@ -48,5 +52,38 @@ fun FollowingPubs(){
     }
 }
 
-//@Composable
-//fun followedPublicationItem(data: )
+@Composable
+fun morePublications(){
+    Column {
+        Text(stringResource(id = R.string.more_publicationa), color = Color.Black, style = MaterialTheme.typography.subtitle1)
+        Image(painter = painterResource(id = R.drawable.ic_more_pub_line), contentDescription = stringResource(
+            id = R.string.underline
+        ))
+        LazyColumn{
+
+        }
+    }
+}
+
+@Composable
+fun followedPublicationItem(){
+    Column(modifier = Modifier.width(87.dp)) {
+        // image
+        // name
+        
+    }
+}
+
+@Composable
+fun morePublicationItem(){
+    Row (modifier = Modifier.height(89.dp)){
+        // image
+        Column (modifier = Modifier.width(237.dp)){
+            // name
+            // description
+            // title of most recent article
+        }
+        // follow button
+
+    }
+}
