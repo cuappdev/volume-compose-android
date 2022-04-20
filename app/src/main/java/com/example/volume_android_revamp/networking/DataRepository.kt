8c017dea6b61a3f81d8d23dacc7a603da9fd4683
aasthashah999 <aasthashah999@gmail.com>
2022-04-19 21:33:03 -0400

    Looks messy but trending and other articles populate and scroll correctly

--- app/src/main/java/com/example/volume_android_revamp/networking/DataRepository.kt
+++ app/src/main/java/com/example/volume_android_revamp/networking/DataRepository.kt
@@ -1,14 +1,14 @@
 package com.example.volume_android_revamp.networking
 
+import com.example.volume_android_revamp.AllArticlesQuery
 import com.example.volume_android_revamp.TrendingArticlesQuery
 
 class DataRepository(private val webService:NetworkingApi) {
 
-    suspend fun fetchAllArticles(){
-        webService.fetchAllArticles()
-    }
+    suspend fun fetchAllArticles(): AllArticlesQuery.Data? =
+        webService.fetchAllArticles().data
 
-    suspend fun fetchTrendingArticles(): TrendingArticlesQuery.Data? {
-       return webService.fetchTrendingArticles().data
-    }
+    suspend fun fetchTrendingArticles(): TrendingArticlesQuery.Data? =
+        webService.fetchTrendingArticles().data
+
 }
--- app/src/main/java/com/example/volume_android_revamp/viewmodels/HomeTabViewModel.kt
+++ app/src/main/java/com/example/volume_android_revamp/viewmodels/HomeTabViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.apollographql.apollo3.api.ApolloResponse
 import com.apollographql.apollo3.exception.ApolloException
+import com.example.volume_android_revamp.AllArticlesQuery
 import com.example.volume_android_revamp.TrendingArticlesQuery
 import com.example.volume_android_revamp.networking.DataRepository
 import com.example.volume_android_revamp.state.State
@@ -17,11 +18,14 @@ import java.lang.Exception
 
 class HomeTabViewModel(private val repository: DataRepository):ViewModel() {
     private val _trendingArticlesState = MutableStateFlow<State<TrendingArticlesQuery.Data>>(State.Empty())
+    private val _allArticlesState = MutableStateFlow<State<AllArticlesQuery.Data>>(State.Empty())
 
     val trendingArticlesState: StateFlow<State<TrendingArticlesQuery.Data>> = _trendingArticlesState
+    val allArticlesState: StateFlow<State<AllArticlesQuery.Data>> = _allArticlesState
 
     init{
         queryTrendingArticles()
+        queryAllArticles()
     }
 
     private fun queryTrendingArticles() = viewModelScope.launch {
@@ -33,6 +37,16 @@ class HomeTabViewModel(private val repository: DataRepository):ViewModel() {
         }catch (e:Exception){
             Log.d("home", e.toString())
             _trendingArticlesState.value = State.Error("There was an error loading trending articles!")
+        }
+    }
+
+    private fun queryAllArticles() = viewModelScope.launch {
+        _allArticlesState.value = State.Loading()
+        try{
+            val response = repository.fetchAllArticles()
+            _allArticlesState.value = response?.let { State.Success(it) }!!
+        } catch (e:Exception){
+            _allArticlesState.value = State.Error("There was an error loading all the articles")
         }
     }
 
