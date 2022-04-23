9358713630fc28ba42eed0a8a09d1861ab03fa96
benha <benharris4848@gmail.com>
2022-04-23 16:21:34 -0400

    Implement messy PublicationsScreen.kt

--- app/src/main/java/com/example/volume_android_revamp/ui/PublicationsScreen.kt
+++ app/src/main/java/com/example/volume_android_revamp/ui/PublicationsScreen.kt
@@ -1,33 +1,54 @@
 package com.example.volume_android_revamp.ui
 
+import android.graphics.drawable.Icon
+import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.volume_android_revamp.AllArticlesQuery
+import com.example.volume_android_revamp.AllPublicationsQuery
 import com.example.volume_android_revamp.R
 import com.example.volume_android_revamp.viewmodels.PublicationTabViewModel
+import com.example.volume_android_revamp.state.State
+
 
 @Composable
 fun PublicationScreen(publicationTabViewModel: PublicationTabViewModel){
-    val context = LocalContext.current
-    Column{
-        publicationsTitle()
-        LazyColumn{
-            item{ followingPubs() }
-            item{ morePublications() }
+    val allPublicationState = publicationTabViewModel.allPublicationsState.collectAsState().value
+    LazyColumn{
+        item { publicationsTitle() }
+        item { morePublications() }
+        allPublicationState.value?.getAllPublications?.let{
+            items(it.size) {
+                when (val allPublicationState =
+                    publicationTabViewModel.allPublicationsState.collectAsState().value){
+                    is State.Success<AllPublicationsQuery.Data> ->
+                        morePublicationItem(data=allPublicationState.value?.getAllPublications?.get(it))
+                    is State.Error<AllPublicationsQuery.Data> -> Log.d("PublicationsTab", "EROROROROROROR aa")
+                    is State.Loading<AllPublicationsQuery.Data> -> Log.d("PublicationsTab", "loading aa")
+                    is State.Empty<AllPublicationsQuery.Data> -> Log.d("PublicationsTab", "empty aa")
+                }
+            }
         }
+//        item{ followingPubs() }
+//        item{ morePublications() }
+
     }
 
 }
@@ -39,51 +60,62 @@ fun publicationsTitle(){
     }
 }
 
-@Composable
-fun followingPubs(){
-    Column {
-        Text(stringResource(id = R.string.following), color = Color.Black, style = MaterialTheme.typography.subtitle1)
-        Image(painter = painterResource(id = R.drawable.ic_following_pub_line), contentDescription = stringResource(
-            id = R.string.underline
-        ))
-        LazyRow{
-
-        }
-    }
-}
+//@Composable
+//fun followingPubs(){
+//    Column {
+//        Text(stringResource(id = R.string.following), color = Color.Black, style = MaterialTheme.typography.subtitle1)
+//        Image(painter = painterResource(id = R.drawable.ic_following_pub_line), contentDescription = stringResource(
+//            id = R.string.underline
+//        ))
+//        LazyRow{
+//
+//        }
+//    }
+//}
 
 @Composable
 fun morePublications(){
-    Column {
+    Box {
         Text(stringResource(id = R.string.more_publicationa), color = Color.Black, style = MaterialTheme.typography.subtitle1)
         Image(painter = painterResource(id = R.drawable.ic_more_pub_line), contentDescription = stringResource(
             id = R.string.underline
         ))
-        LazyColumn{
 
-        }
     }
 }
 
-@Composable
-fun followedPublicationItem(){
-    Column(modifier = Modifier.width(87.dp)) {
-        // image
-        // name
-        
-    }
-}
+//@Composable
+//fun followedPublicationItem(){
+//    Column(modifier = Modifier.width(87.dp)) {
+//        // image
+//        // name
+//
+//    }
+//}
 
 @Composable
-fun morePublicationItem(){
+fun morePublicationItem(data : AllPublicationsQuery.GetAllPublication?){
     Row (modifier = Modifier.height(89.dp)){
-        // image
-        Column (modifier = Modifier.width(237.dp)){
-            // name
-            // description
-            // title of most recent article
+        if (data != null){
+            AsyncImage(model = data.profileImageURL, contentDescription = null, Modifier
+                .height(50.dp)
+                .width(50.dp)
+                .clip(CircleShape), contentScale = ContentScale.Crop)
         }
-        // follow button
+        Column (modifier = Modifier.width(237.dp)){
+            if (data !=null){
+                Text(text = data.name)
+                Text(text = data.bio)
+                data.mostRecentArticle?.title?.let { Text(text = truncateTitle(it)) }
+            }
+        }
+        Button(
+            onClick = { }
+        ){
+            Image(painter = painterResource(id = R.drawable.follow_small), contentDescription = null)
+        }
 
     }
 }
+
+fun truncateTitle(title: String):String = if (title.length > 57) (title.substring(0, 57) + "...") else title
--- app/src/main/java/com/example/volume_android_revamp/viewmodels/PublicationTabViewModel.kt
+++ app/src/main/java/com/example/volume_android_revamp/viewmodels/PublicationTabViewModel.kt
@@ -1,7 +1,31 @@
 package com.example.volume_android_revamp.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.volume_android_revamp.AllPublicationsQuery
 import com.example.volume_android_revamp.networking.DataRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import com.example.volume_android_revamp.state.State
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
 
 class PublicationTabViewModel(private val repository: DataRepository): ViewModel() {
+    private val _allPublicationsState = MutableStateFlow<State<AllPublicationsQuery.Data>>(State.Empty())
+
+    val allPublicationsState: StateFlow<State<AllPublicationsQuery.Data>> = _allPublicationsState
+
+    init{
+        queryAllPublications()
+    }
+
+    private fun queryAllPublications() = viewModelScope.launch {
+        _allPublicationsState.value = State.Loading()
+        try {
+            val response = repository.fetchAllPublications()
+            _allPublicationsState.value = response?.let { State.Success(it) }!!
+        }catch (e:Exception){
+            _allPublicationsState.value = State.Error("There was an error loading all the publications")
+        }
+    }
 }
