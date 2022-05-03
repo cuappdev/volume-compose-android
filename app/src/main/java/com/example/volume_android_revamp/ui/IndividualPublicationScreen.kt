07a4fd1a767871d52590410151b06c9faf074eea
emilyhu8 <91348233+emilyhu8@users.noreply.github.com>
2022-05-03 17:39:38 -0400

    Basic representation of the screen

--- app/src/main/java/com/example/volume_android_revamp/ui/IndividualPublicationScreen.kt
+++ app/src/main/java/com/example/volume_android_revamp/ui/IndividualPublicationScreen.kt
@@ -1,27 +1,40 @@
 package com.example.volume_android_revamp.ui
 
+import android.content.ClipData
+import android.media.Image
 import android.util.Log
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import com.example.volume_android_revamp.AllArticlesQuery
-import com.example.volume_android_revamp.PublicationByIDQuery
-import com.example.volume_android_revamp.TrendingArticlesQuery
-import com.example.volume_android_revamp.otherArticleItem
+import com.example.volume_android_revamp.*
+import com.example.volume_android_revamp.R
 import com.example.volume_android_revamp.state.State
 import com.example.volume_android_revamp.type.Article
 import com.example.volume_android_revamp.type.Publication
 import com.example.volume_android_revamp.viewmodels.HomeTabViewModel
 import com.example.volume_android_revamp.viewmodels.IndividualPublicationViewModel
+import org.w3c.dom.Text
 
+//"61980a202fef10d6b7f20747"
 @Composable
 fun IndividualPublicationScreen(individualPublicationViewModel: IndividualPublicationViewModel) {
     val publicationByIDState =
@@ -32,11 +45,11 @@ fun IndividualPublicationScreen(individualPublicationViewModel: IndividualPublic
 
                 when (val publicationByIDState =
                     individualPublicationViewModel.publicationByIDState.collectAsState().value) {
-                    //is State.Success<PublicationByIDQuery.Data> ->
-                        //publicationByIDState.value!!.getPublicationByID?.let { it1 -> PublicationBackground(data = it1) }
+                    is State.Success<PublicationByIDQuery.Data> ->
+                        publicationByIDState.value!!.getPublicationByID?.let { it1 -> PublicationScreen(data = it1) }
                     is State.Error<PublicationByIDQuery.Data> -> Log.d(
                         "HomeTab",
-                        "EROROROROROROR aa"
+                        "EROROROROROROR"
                     )
                     is State.Loading<PublicationByIDQuery.Data> -> Log.d("HomeTab", "loading aa")
                     is State.Empty<PublicationByIDQuery.Data> -> Log.d("HomeTab", "empty aa")
@@ -44,25 +57,182 @@ fun IndividualPublicationScreen(individualPublicationViewModel: IndividualPublic
 
             }
         }
+        item(){
+            when (val publicationByIDState =
+                individualPublicationViewModel.publicationByIDState.collectAsState().value) {
+                is State.Success<PublicationByIDQuery.Data> ->
+                    publicationByIDState.value!!.getPublicationByID?.let { it1 -> PublicationRecent(data = it1) }
+                is State.Error<PublicationByIDQuery.Data> -> Log.d(
+                    "HomeTab",
+                    "EROROROROROROR"
+                )
+                is State.Loading<PublicationByIDQuery.Data> -> Log.d("HomeTab", "loading aa")
+                is State.Empty<PublicationByIDQuery.Data> -> Log.d("HomeTab", "empty aa")
+            }
+        }
     }
 }
-//PublicationByIDQuery.GetPublicationByID
-//.backgroundImageURL
+//data: PublicationByIDQuery.GetPublicationByID
 @Composable
-fun PublicationBackground(data: String) {
+fun PublicationScreen(data: PublicationByIDQuery.GetPublicationByID) {
+    //val data= PublicationByIDQuery.GetPublicationByID(data1)
     Column {
-        AsyncImage(
-            model = data, modifier = Modifier
+        Box(modifier=Modifier.height(400.dp)){
+            AsyncImage(
+                model = data.backgroundImageURL, modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    , contentDescription = null, contentScale = ContentScale.Crop)
+            AsyncImage(model = data.profileImageURL, modifier= Modifier
                 .height(180.dp)
-                .width(180.dp), contentDescription = null, contentScale = ContentScale.Crop
-        )
+                .padding(top = 60.dp, start = 20.dp)
+                .clip(CircleShape), contentDescription = null, contentScale = ContentScale.Crop)
+            Row{
+                Text(data.name, fontSize = 20.sp);
+                Image(painter= painterResource(id = R.drawable.ic_follow),
+                    contentDescription = "", modifier= Modifier.padding(start = 50.dp)
+                )
+            }
+            Row(modifier=Modifier.padding(top=80.dp)){
+                Text(data.numArticles.toString()+" -Articles");
+                Text(data.shoutouts.toString()+" -Shout-outs", modifier=Modifier.padding(start=5.dp));
+            }
+            Text(data.bio, modifier=Modifier.padding(top=100.dp), fontSize = 15.sp)
+            //Social Medias
+            Publicationsocial(data)
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_divider),
+                    contentDescription = "", modifier = Modifier.padding(top = 75.dp)
+                )
+            }
+            Box(modifier=Modifier.padding(top=140.dp)){
+                Text("Articles")
+                Image(
+                    painter = painterResource(id = R.drawable.ic_underline),
+                    contentDescription = "", modifier = Modifier.padding(top = 75.dp)
+                )
+            }
+
+            //print out the most recent articles?? didnt get a list from backend
+            for (i in 1..data.numArticles.toInt()){
+                PublicationRecent(data)
+             }
+
+        }
+
+
+
     }
 }
 
+//data: PublicationByIDQuery.GetPublicationByID
+@Composable
+fun PublicationProfilePic(data: PublicationByIDQuery.GetPublicationByID){
+    Column{
+        AsyncImage(model = data.profileImageURL, modifier= Modifier
+            .height(180.dp)
+            .width(180.dp), contentDescription = null, contentScale = ContentScale.Crop)
+    }
+}
+
+@Composable
+fun PublicationRecent(data: PublicationByIDQuery.GetPublicationByID){
+    Column{
+        val recent=data.mostRecentArticle
+        Box(){
+            Column() {
+                Row() {
+                    Text(recent!!.title, fontSize=20.sp)
+                    AsyncImage(
+                        model = recent.imageURL,
+                        modifier = Modifier
+                            .height(180.dp)
+                            .width(180.dp)
+                            .padding(start = 50.dp),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop
+                    )
+                }
+                Row(modifier=Modifier.padding(20.dp)){
+                    Text(recent!!.date.toString()+" • "+recent.shoutouts.toString()+"shout-outs")
+                }
+
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Publicationsocial(data: PublicationByIDQuery.GetPublicationByID){
+    // Creating an annonated string
+    Row(){
+        Image(
+            painter = painterResource(id = R.drawable.ic_facebook),
+            contentDescription = "", modifier = Modifier.height(5.dp)
+        )
+        val mAnnotatedLinkString = buildAnnotatedString {
+
+            // creating a string to display in the Text
+            val mStr = "Instagram"
+
+            // word and span to be hyperlinked
+            val mStartIndex = 0
+            val mEndIndex = 8
+
+            append(mStr)
+            addStyle(
+                style = SpanStyle(
+                    color = Color.Blue,
+                    textDecoration = TextDecoration.Underline
+                ), start = mStartIndex, end = mEndIndex
+            )
+
+            // attach a string annotation that
+            // stores a URL to the text "link"
+            addStringAnnotation(
+                tag = "URL",
+                annotation = data.socials.toString(),
+                start = mStartIndex,
+                end = mEndIndex
+            )
+
+        }
+
+        // UriHandler parse and opens URI inside
+        // AnnotatedString Item in Browse
+        val mUriHandler = LocalUriHandler.current
+
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(2.dp)) {
+
+            // ???? Clickable text returns position of text
+            // that is clicked in onClick callback
+            ClickableText(
+                text = mAnnotatedLinkString,
+                onClick = {
+                    mAnnotatedLinkString
+                        .getStringAnnotations("URL", it, it)
+                        .firstOrNull()?.let { stringAnnotation ->
+                            mUriHandler.openUri(stringAnnotation.item)
+                        }
+                }
+            )
+        }
+    }
+
+}
+
+
+/**
 @Preview
 @Composable
 fun previewPage(){
     PublicationBackground("61980a202fef10d6b7f20747");
 }
+ */
 
 
